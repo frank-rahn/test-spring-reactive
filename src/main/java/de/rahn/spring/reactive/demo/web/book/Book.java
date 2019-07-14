fb1ae0df68150caeabfb1fd82509db4666474e21
Frank W. Rahn <frank@frank-rahn.de>
2019-07-14 14:27:39 +0200

    Copyright hinzugefügt

--- src/main/java/de/rahn/spring/reactive/demo/web/book/Book.java
+++ src/main/java/de/rahn/spring/reactive/demo/web/book/Book.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.rahn.spring.reactive.demo.web.book;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -5,11 +21,11 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class Book {
 
-  private long id;
+  private final long id;
 
-  private String name;
+  private final String name;
 
-  public Book(long id, String name) {
+  Book(long id, String name) {
     this.id = id;
     this.name = name;
   }
